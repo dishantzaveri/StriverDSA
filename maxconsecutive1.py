fd9c624bc27c6551e0089962d7ecc68e72b4eb30
dishantzaveri <zaverdishant@gmail.com>
2025-10-02 01:14:53 -0500

    day 2

---
+++ maxconsecutive1.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        maxi=0
+        cnt=0
+        for i in range(len(nums)):
+            if nums[i]==1:
+                cnt+=1
+            else: 
+                cnt=0
+            maxi=max(maxi,cnt)
+        return maxi
+        
+        
+
+if __name__ == "__main__":
+    nums = [1, 1, 0, 1, 0, 0]
+    obj = Solution()
+    ans = obj.findMaxConsecutiveOnes(nums)
+    print("The maximum  consecutive 1's are", ans)
